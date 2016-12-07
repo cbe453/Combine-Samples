5228fb4af795c031e53a2a956bce911769704d01
cbe453 <cbe453@mail.usask.ca>
2016-12-07 12:45:26 -0600

    nearly complete version of combine-samples.sh

--- combine-samples.sh
+++ combine-samples.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
 echo "Combining samples"
+
+while read line
+do
+	echo $line
+	filestocat=`ls 2>miss.txt | grep "$line-" 2> miss.txt | grep 'besthits.sam'`
+	
+	echo $filestocat
+
+	echo cat $filestocat to ../new-align/$line\_besthits.sam
+
+	if test #? = 0
+	then 
+		echo "successful concatenation of $line"
+	fi 
+done
